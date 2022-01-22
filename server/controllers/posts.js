05753d4fdd126ef871d3b6bd01cf3ddcee8b8dea
scarycool11 <79058996+scarycool11@users.noreply.github.com>
2022-01-23 06:15:19 +0900

    Setup Posts Methods

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,22 @@
+import PostMessage from '../models/postMessage.js'
 
 export const getPosts = (req, res) => {
-    res.send('THIS WORKS!');
+    try {
+        const postMessages = await PostMessage.find();
+   
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message:  error.message });
+    }
+
+
+
+}
+
+
+
+export const createPost = (req, res) => {
+    res.send('Post Creation')
 }
