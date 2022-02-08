866bac656f2a789755b73411b3872b1bf48aecec
Keon <dawsonkeon@yahoo.com>
2022-02-08 22:41:56 +0900

    Working without bugs before Filebase64 import

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
+import {TextField, Button, Typography, Paper } from '@material-ui/core';
+
 import useStyles from './styles'
 
-
 const Form = () => {
-    const classes = useStyles()
+    const [postData, setPostData ] = useState({ creator: '', title: '', message:'', tags:'', selectedFile:'' });
+    const classes = useStyles();
+    
+    const handleSubmit = () => {
+
+    }
+    
     return (
-        <h1>FORM</h1>
+        <Paper class={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant="h6">Creating a memory</Typography>
+            <TextField name="creator" variant="outlined" label="creator" fullWidth  value={postData.creator} onChange={(e) => setPostData({ ... postData, creator: e.target.value })} />
+            <TextField name="title" variant="outlined" label="title" fullWidth  value={postData.title} onChange={(e) => setPostData({ ... postData, title: e.target.value })} />
+            <TextField name="message" variant="outlined" label="message" fullWidth  value={postData.message} onChange={(e) => setPostData({ ... postData, message: e.target.value })} />
+            <TextField name="tags" variant="outlined" label="tags" fullWidth  value={postData.tags} onChange={(e) => setPostData({ ... postData, tags: e.target.value })} />
+            <div className={classes.fileInput}>
+
+            </div>
+            </form>
+        </Paper>
     );
 }
 
