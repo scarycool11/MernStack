9cc6eb63d9f216e37c44e298ad63af7112748cba
Keon <dawsonkeon@yahoo.com>
2022-02-11 11:52:01 +0900

    Added Like and Delete Functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles()
     return (
         <Card className={classes.card}>
@@ -16,11 +16,28 @@ const Post = ({ post }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()} </Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} sixe="small" onClick={() => {}}>
+                <Button style={{color: 'white'}} sixe="small" onClick={() => setCurrentId (post._id)}>
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
-            
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+             </div>
+            <CardContent>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                    {post.likeCount}
+                </Button>
+            </CardActions>
         </Card>
     );
 }
